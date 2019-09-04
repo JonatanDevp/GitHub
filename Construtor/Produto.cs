f5fd97191e9f0eb49cf55bafbc57ac2fa080356e
jonat <jonat@DESKTOP-QFG8A7A>
2019-09-03 22:17:53 -0300

    Adicionar arquivos de projeto.

---
+++ Construtor/Produto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Construtor
+{
+    class Produto
+    {
+        private string _nome;  //tem um lógica especifica
+        public double _preco { get; private set; } //propriedade autoimplementada
+        public int Quantidade { get; private set; } 
+
+
+        public Produto()
+        {
+
+        }
+
+        //public Produto(string nome, double preco, int quantidade)  //construtores
+        //{
+        //    _nome = nome;
+        //    _preco = preco;
+        //    Quantidade = quantidade;
+        //}
+
+        //public Produto(string nome, double preco)
+        //{
+        //    _nome = nome;
+        //    _preco = preco;
+        //    Quantidade = 5 ;
+        //}
+
+        public string Nome  //propriedade maneira correta de encapsulamento no C#
+        {
+            get { return _nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+            set
+            {
+                if(Preco > 0)
+                {
+                    _preco = Preco; 
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Construtor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+
+            p1.Nome = "notebook";
+            Console.WriteLine(p1.Nome);
+        }  
+    }
+}
